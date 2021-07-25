cf1db062ebe04656ac9b2a5caf9911a5877b6f3d
Justyna Postrozny <justyna.pietryga@gmail.com>
2021-07-25 04:26:28 +0200

    Modify the modules the modules tree

--- src/main/java/com/mobilemall/general/ScrapperApplication.java
+++ src/main/java/com/mobilemall/general/ScrapperApplication.java
@@ -1,16 +1,16 @@
-package com.mobilemall.general;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
-@SpringBootApplication
-@ComponentScan(value = {"com.mobilemall.*"})
-@Slf4j
-public class ScrapperApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(ScrapperApplication.class, args);
-    }
-}
+//package com.mobilemall.general;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.ComponentScan;
+//
+//@SpringBootApplication
+//@ComponentScan(value = {"com.mobilemall.*"})
+//@Slf4j
+//public class ScrapperApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(ScrapperApplication.class, args);
+//    }
+//}
--- src/main/java/com/mobilemall/general/controllers/MallController.java
+++ src/main/java/com/mobilemall/general/controllers/MallController.java
@@ -1,56 +1,64 @@
-package com.mobilemall.general.controllers;
-
-import com.mobilemall.scrapper.conf.ShopsEnum;
-import com.mobilemall.scrapper.scrap_classes.Scrapable;
-import com.mobilemall.scrapper.model.Category;
-import lombok.val;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.stream.Collectors;
-
-import static java.util.function.Function.identity;
-
-@RestController
-@RequestMapping(path = "/api")
-public class MallController {
-
-    private final Map<ShopsEnum, Scrapable> scrapperHandler;
-
-    @Autowired
-    public MallController(Map<ShopsEnum, Scrapable> scrapperHandler) {
-        this.scrapperHandler = scrapperHandler;
-    }
-
-    @GetMapping(path = "/categories")
-    @ResponseStatus(HttpStatus.OK)
-    public Map<ShopsEnum, List<Category>> getCategories(@RequestParam Set<ShopsEnum> shopList) {
-        return shopList.parallelStream()
-                .collect(Collectors.toMap(identity(), shop -> {
-//                    List<Category> categories = List.of();
-//                    ExecutorService es = Executors.newSingleThreadExecutor();
-//                    Future<List<Category>> result =
-//                            es.submit(() -> scrapperHandler.get(shop).getScrappedCategories());
-//                    try {
-//                        categories = result.get();
-//                    } catch (Exception e) {
-//                        System.out.println("Failed");
-//                    }
-//                    es.shutdown();
-//                    return categories;
-                    return scrapperHandler.get(shop).getScrappedCategories();
-                }));
-    }
-
-    //TODO handle the situation when scrapping from one shop would fail
-    //TODO Introduce React Java or sth else
-    //TODO Consider removing WebDriver as a bean and create seperate instance for every run and make calls in parallel
-}
+//package com.mobilemall.general.controllers;
+//
+//import com.mobilemall.scrapper.conf.ShopsEnum;
+//import com.mobilemall.scrapper.model.Category;
+//import com.mobilemall.scrapper.model.Product;
+//import com.mobilemall.scrapper.scrap_classes.Scrapable;
+//import lombok.val;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.MediaType;
+//import org.springframework.web.bind.annotation.*;
+//import reactor.core.publisher.Flux;
+//import reactor.core.scheduler.Schedulers;
+//
+//import java.util.ArrayList;
+//import java.util.Map;
+//import java.util.Set;
+//
+//@RestController
+//@RequestMapping(path = "/api")
+//public class MallController {
+//
+//    private final Map<ShopsEnum, Scrapable> scrapperHandler;
+//
+//    @Autowired
+//    public MallController(Map<ShopsEnum, Scrapable> scrapperHandler) {
+//        this.scrapperHandler = scrapperHandler;
+//    }
+//
+//    @GetMapping(path = "/categories", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+//    @ResponseStatus(HttpStatus.OK)
+//    public Flux<Category> getCategories(@RequestParam Set<ShopsEnum> shopList) {
+//        return Flux.fromIterable(shopList)
+//                .subscribeOn(Schedulers.boundedElastic())
+//                .flatMap(shop -> scrapperHandler.get(shop)
+//                        .getScrappedCategories()
+//                        .subscribeOn(Schedulers.boundedElastic()));
+//    }
+//
+//    @GetMapping(path = "/products", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+//    @ResponseStatus(HttpStatus.OK)
+//    public Flux<Product> getProducts() {
+//        return scrapperHandler
+//                .get(ShopsEnum.RESERVED)
+//                .getProducts(Category.builder()
+//                        .url("https://www.reserved.com/pl/pl/woman/clothes/blouses")
+////                        .url("https://www.bershka.com/pl/kobieta/odzie%C5%BC/bluzy-c1010193222.html")
+//                        .build());
+//    }
+//
+//    @GetMapping(path = "/test", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+////    @ResponseStatus(HttpStatus.OK)
+//    public Flux<String> getTest() {
+//        val array = new ArrayList<String>();
+//        for (int i = 0; i <= 1000; i++) {
+//            array.add(String.valueOf(i));
+//        }
+//        return Flux.fromIterable(array);
+//    }
+//
+//    //TODO handle the situation when scrapping from one shop would fail
+//    //TODO Introduce React Java or sth else
+//    //TODO Consider removing WebDriver as a bean and create seperate instance for every run and make calls in parallel
+//}
